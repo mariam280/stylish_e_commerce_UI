36c199e031c17b253b2614975bc8d8f9c264dfbd
mariam ibrahim <neamamohammed649@gmail.com>
2025-08-16 15:42:17 +0300

    create custom dots indicator

---
+++ lib/modules/onBoarding/presentation/widgets/custom_dot.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class CustomDotIndicator extends StatelessWidget {
+  const CustomDotIndicator({super.key, required this.isActive});
+
+  final bool isActive;
+  @override
+  Widget build(BuildContext context) {
+    return AnimatedContainer(
+      duration: const Duration(milliseconds: 300),
+      width: isActive ? 40 : 6,
+      height: 8,
+      decoration: ShapeDecoration(
+        color: isActive ? Colors.black : const Color(0xFF707070),
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(12),
+        ),
+      ),
+    );
+  }
+}
