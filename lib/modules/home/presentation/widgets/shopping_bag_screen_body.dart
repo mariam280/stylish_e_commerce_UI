e7a118b5364629a43f8d57ef8bd3fb73a06b21c0
mariam ibrahim <neamamohammed649@gmail.com>
2025-08-19 22:55:16 +0300

    create order payment details

--- lib/modules/home/presentation/widgets/shopping_bag_screen_body.dart
+++ lib/modules/home/presentation/widgets/shopping_bag_screen_body.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:stylish_e_commerce/core/constants/app_styles.dart';
 import 'package:stylish_e_commerce/modules/home/presentation/widgets/card_details_header.dart';
 import 'package:stylish_e_commerce/modules/home/presentation/widgets/shopping_bag_card.dart';
 import 'package:stylish_e_commerce/modules/home/presentation/widgets/shopping_bag_coupon.dart';
@@ -16,8 +17,73 @@ class ShoppingBagScreenBody extends StatelessWidget {
           CardDetailsHeader(),
           ShoppingBagCard(),
           ShoppingBagCoupon(),
+          Divider(),
+          OrderPaymentDetail(),
+           Divider(),
         ],
       ),
     );
   }
 }
+
+class OrderPaymentDetail extends StatelessWidget {
+  const OrderPaymentDetail({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Text(
+          'Order Payment Details',
+          style: AppStyles.stylesMedium16(context).copyWith(fontSize: 17),
+        ),
+        Row(
+          children: [
+            Text(
+              'Order Amounts',
+              style: AppStyles.stylesRegular14(context)
+                  .copyWith(fontSize: 16, color: Colors.black),
+            ),
+            Spacer(),
+            Text(r'$ 7,000.00',style: AppStyles.stylesemiBold18(context).copyWith(color: Colors.black),),
+          ],
+        ),
+        Row(
+          children: [
+            Text('Convenience',style: AppStyles.stylesRegular14(context)
+                  .copyWith(fontSize: 16, color: Colors.black),),
+            Text('Know More',style: AppStyles.stylesemiBold18(context).copyWith(fontSize: 12),),
+            Spacer(),
+            Text('Apply Coupon',style: AppStyles.stylesemiBold18(context).copyWith(fontSize: 12),),
+          ],
+        ),
+        Row(
+          children: [
+            Text('Delivery Fee',style: AppStyles.stylesRegular14(context).copyWith(
+              color: Colors.black
+            ),),
+            Spacer(),
+            Text('Free',style: AppStyles.stylesemiBold18(context).copyWith(fontSize: 12),),
+          ],
+        )
+      ],
+    );
+  }
+}
+
+// class OrderPaymentDetailsText extends StatelessWidget {
+//   const OrderPaymentDetailsText({super.key, required this.title, required this.subTitle});
+//   final String title, subTitle;
+//   @override
+//   Widget build(BuildContext context) {
+//     return Row(
+//       children: [
+//         Text(
+//           title,
+//         ),
+//         Spacer(),
+//         Text(subTitle),
+//       ],
+//     );
+//   }
+// }
